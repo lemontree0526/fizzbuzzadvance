8a3fc1573d8e9d3d59950d52e69181360931eebb
zhiheng.ma <zhiheng.ma@thoughtworks.com>
2022-05-30 01:46:03 +0800

    should_return_FizzBuzzWhizz_when_playing_game_given_number_is_mod_by_3_5_7_and_not_container_3_5_7

--- fizzbuzz/src/fizzbuzz.py
+++ fizzbuzz/src/fizzbuzz.py
@@ -3,6 +3,14 @@ class FizzBuzzWhizz:
         self.number = number
 
     def game_num(self):
+        if self.number % 3 == 0 and self.number % 7 == 0 and self.number % 5 == 0:
+            return "FizzBuzzWhizz"
+        if self.number % 3 == 0 and self.number % 5 == 0:
+            return "FizzBuzz"
+        if self.number % 3 == 0 and self.number % 7 == 0:
+            return "FizzWhizz"
+        if self.number % 5 == 0 and self.number % 7 ==0:
+            return "BuzzWhizz"
         if self.number % 3 == 0:
             return "Fizz"
         if self.number % 5 == 0:
--- fizzbuzz/test/fizzbuzzadvance.py
+++ fizzbuzz/test/fizzbuzzadvance.py
@@ -14,6 +14,18 @@ def test_should_return_Fizz_when_playing_game_given_number_is_only_mod_by_5_and_
     assert fbw.game_num() == "Buzz"
 
 
-def test_should_return_Whizz_when_playing_game_given_number_is_only_mod_by_7_and_not_container_3_5_7():
-    fbw = FizzBuzzWhizz(int(14))
-    assert fbw.game_num() == "Whizz"
+def test_should_return_FizzBuzz_when_playing_game_given_number_is_only_mod_by_3_5_and_not_container_3_5_7():
+    fbw = FizzBuzzWhizz(int(60))
+    assert fbw.game_num() == "FizzBuzz"
+
+def test_should_return_FizzWhizz_when_playing_game_given_number_is_only_mod_by_3_7_and_not_container_3_5_7():
+    fbw = FizzBuzzWhizz(int(21))
+    assert fbw.game_num() == "FizzWhizz"
+
+def test_should_return_BuzzWhizz_when_playing_game_given_number_is_only_mod_by_5_7_and_not_container_3_5_7():
+    fbw = FizzBuzzWhizz(int(140))
+    assert fbw.game_num() == "BuzzWhizz"
+
+def test_should_return_FizzBuzzWhizz_when_playing_game_given_number_is_mod_by_3_5_7_and_not_container_3_5_7():
+    fbw = FizzBuzzWhizz(int(210))
+    assert fbw.game_num() == "FizzBuzzWhizz"
